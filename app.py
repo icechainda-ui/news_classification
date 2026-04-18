847b2759ff78bcba515dcfba8ea8134d95e3f211
icechainda-ui <icechainda@gmail.com>
2026-04-18 05:43:17 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-from ml_core import train_model
+from ml_core import train_model_for_app
 
 st.set_page_config(
     page_title="News Analytics Dashboard",
@@ -248,7 +248,7 @@ section[data-testid="stSidebar"] * {
 model, encoder = None, None
 model_loaded = False
 try:
-    model, encoder = train_model()
+    model, encoder = train_model_for_app("train_small.csv")
     model_loaded = True
 except Exception:
     model_loaded = False
